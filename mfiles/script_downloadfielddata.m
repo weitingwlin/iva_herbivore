ebf64bf604e806a7d3e91b5fc0334008716b14b2
wildwolflin <weiting.lin.lin@gmail.com>
2017-09-11 11:47:16 -0500

    add save data to /data

--- mfiles/script_downloadfielddata.m
+++ mfiles/script_downloadfielddata.m
@@ -1,2 +1,3 @@
 %% save data
- urlwrite('https://portal.lternet.edu/nis/dataviewer?packageid=knb-lter-gce.583.22&entityid=8ae016d3046ae5e672512cc133818d49','bugdata2014.csv');
+ urlwrite('https://portal.lternet.edu/nis/dataviewer?packageid=knb-lter-gce.583.22&entityid=8ae016d3046ae5e672512cc133818d49', ...
+             './data/bugdata2014.csv');
